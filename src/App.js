304fe33014707a33b1af0211d424e5a0b515eea9
kavatibhagya <bhagyalakshmi.kavati@gmail.com>
2020-10-10 12:39:46 -0700

    Added sass and removed grommet

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
-import { Grommet } from 'grommet';
 import theme from './theme';
 
 function App() {
   return (
-    <Grommet theme={theme}>
-      <header>Welcome to weather app!</header>
-    </Grommet>
+    <>
+    <header class="header">
+
+    </header>
+    <main class="main">
+      
+    </main>
+    <footer>
+
+    </footer>
+    </>
   );
 }
 
