6b32e68479277c572d666371109edb74c72a17f1
kavatibhagya <bhagyalakshmi.kavati@gmail.com>
2020-10-09 17:24:50 -0700

    Added locations

---
+++ src/config/locations.js
@@ -0,0 +1,21 @@
+const locations = [{
+  cityName: 'San Jose',
+  stateCode: 'CA',
+  countryCode: 'USA',
+}, {
+  cityName: 'San Francisco',
+  stateCode: 'CA',
+  countryCode: 'USA',
+},{
+  cityName: 'New York',
+  stateCode: 'NY',
+  countryCode: 'USA',
+},{
+  cityName: 'London',
+  stateCode: '',
+  countryCode: 'GB',
+},{
+  cityName: 'Paris',
+  stateCode: '',
+  countryCode: 'FR',
+}]
