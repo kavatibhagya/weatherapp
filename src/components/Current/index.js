88f673cad39b8199a4d434852247967bbf7414c5
kavatibhagya <bhagyalakshmi.kavati@gmail.com>
2020-10-10 21:43:40 -0700

    Added current temp, feels like and humidity

--- src/components/Current/index.js
+++ src/components/Current/index.js
@@ -1,9 +1,38 @@
 import React from 'react';
+import './styles.scss';
 
 function Current({weather, location}) {
+  
+  const getDate = (tempDate) => {
+    const date = new Date();
+    date.setMilliseconds(tempDate);
+
+    return date.toLocaleDateString();
+  }
+
+
   return (
     <div className="current">
-      <h1>Current temparature at {location.cityName}, {location.stateCode} is {weather.temp}</h1>
+      <div className="current__main">
+        <h1>Current temparature at {location.cityName}, {location.stateCode} is {weather.temp}</h1>
+        <h2>as of {getDate(weather.dt)}</h2>
+      </div>
+      <div className="current__additional">
+        <div>
+          <div className="current__item">
+            <div>Feels like: </div>
+            <div>{weather.feels_like} </div>
+          </div>
+          <div className="current__item">
+            <div>Humidity: </div>
+            <div>{weather.humidity}%</div>
+          </div>
+          <div className="current__item">
+            <div>Wind speed: </div>
+            <div>{weather.wind_speed} mph</div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
