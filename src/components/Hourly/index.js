9fdcaa523f2a6ef0be6ff7206f2e92e5c3a095ea
kavatibhagya <bhagyalakshmi.kavati@gmail.com>
2020-10-17 16:48:38 -0700

    Showing hourly forecast

--- src/components/Hourly/index.js
+++ src/components/Hourly/index.js
@@ -3,8 +3,14 @@ import './styles.scss';
 
 function Hourly({ weather }) {
   return(
-    <div>
-      Hourly
+    <div className="hourly">
+      {
+        weather.map((forecast) => (
+          <div className="hourly__card">
+            <div className="hourly__temp">{forecast.temp}</div>
+          </div>
+        ))
+      }
     </div>
   )
 }
