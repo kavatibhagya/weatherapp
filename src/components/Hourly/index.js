0d7cbd3aab6ee752d15199dc883f3559225aa230
kavatibhagya <bhagyalakshmi.kavati@gmail.com>
2020-10-25 12:15:15 -0700

    Added get date to util

--- src/components/Hourly/index.js
+++ src/components/Hourly/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import './styles.scss';
+import dateTime from '../../utils/date';
+const { getDate, getTime } = dateTime;
 
 function Hourly({ weather }) {
   return(
     <div className="hourly">
       {
-        weather.map((forecast) => (
-          <div className="hourly__card">
+        weather.map((forecast, index) => (
+          <div key={index} className="hourly__card">
             <div className="hourly__temp">{forecast.temp}</div>
+            <div>{getDate(forecast.dt)}</div>
+            <div className="hourly__time">{getTime(forecast.dt)}</div>
           </div>
         ))
       }
--- src/utils/date.js
+++ src/utils/date.js
@@ -1,17 +1,42 @@
+const getDate = (unixTime, year = false) => {
+  let res;
+  const date = new Date(unixTime * 1000);
+  const month = date.getMonth();
+  const dat = date.getDate();
+  const yr = date.getFullYear();
+
+  res = `${month}/${dat}`;
+
+  if(year) {
+    res = `${res}/${yr}`;
+  }
+
+  return res;
+}
+
 const getTime = (unixTime, sec = false) => {
   let res;
   const date = new Date(unixTime * 1000);
   const hours = date.getHours();
   const minites = date.getMinutes() < 9 ? `0${date.getMinutes()}` : date.getMinutes();
   const seconds = date.getSeconds() < 9 ? `0${date.getSeconds()}` : date.getSeconds();
+  const mer = hours < 12 ? 'AM' : 'PM';
+  const hrs = hours > 12 ? hours - 12 : hours;
 
-  res = `${hours} : ${minites}` ;
+  res = `${hrs}:${minites}` ;
   
   if(sec) {
-    res = `${res} : ${seconds}`  
+    res = `${res}:${seconds}`  
   }
+
+  res = `${res} ${mer}`;
 
   return res;
 }
 
-export default getTime;
+const dateTime = {
+  getDate,
+  getTime,
+}
+
+export default dateTime;
